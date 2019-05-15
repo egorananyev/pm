84229bd2d739a7088f256fdfcd762a59c67c7395
egorananyev <egor.ananyev@gmail.com>
2019-05-15 18:31:44 +0800

    Ensured correct monitor and stimulus dimensions and timing. Coded the response phase.

--- pm.py
+++ pm.py
@@ -11,7 +11,6 @@ from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
 from psychopy import visual, core, event, gui, sound, monitors
 import numpy as np
 import os
-from subprocess import call  # for running shell commands from within Python
 from psychopy.data import TrialHandler, importConditions
 import pandas as pd
 from datetime import datetime
@@ -23,11 +22,11 @@ debug = True
 # experiment variables:
 exp_name = 'pm1'
 # stimulus parameters:
-stim_diam = 5
-stim_sf = 5
+stim_diam = 5  # for Shocky's Samsung monitor, a 5 deg stim = 6.3 cm
+stim_sf = 1  # cycles per degree (e.g., for a 5 deg stim, there will be 5 cycles)
 # timing variables (note that the number of frames will differ for 60 and 100 Hz refresh rates):
 fix_dur = 20  # in frames
-stim_dur = 2
+stim_dur = 60  # 2
 beg_buff = 10  # beginning buffer of fixation
 end_buff = beg_buff
 wiggle = 10  # additional 'wiggle room' for jittering of stimulus onset
@@ -36,17 +35,30 @@ interval_dur = stim_dur * 2 + beg_buff + end_buff + wiggle  # 34 frames, or 340 
 # trial duration:
 trial_dur = fix_dur + interval_dur * 2  # 88 frames
 
+# Reassign stimulus durations for the slower refresh rate:
 if shocky:
-    ds = 61
-    # dd = (17.2, 12.9)  # display dimensions in cm
-    dr = (1152, 864)  # display resolution in px
-    dd = (34.4, 25.8)  # display dimensions in cm
-    windowed = True
+    fix_dur = fix_dur / 2  # in frames
+    stim_dur = stim_dur / 2
+    beg_buff = beg_buff / 2  # beginning buffer of fixation
+    end_buff = beg_buff
+    wiggle = wiggle / 2  # additional 'wiggle room' for jittering of stimulus onset
+    interval_dur = stim_dur * 2 + beg_buff + end_buff + wiggle  # 17 frames
+    trial_dur = fix_dur + interval_dur * 2  # 44 frames
+
+if shocky:
+    ## These are here just for the record -- monitor dimensions are stored in PsychoPy monitor center
+    # ds = 72
+    # dr = (1920, 1080)  # display resolution in px
+    # dd = (51.6, 29.2)  # office Samsung monitor
+    screen_name = 'samsung'
+    full_screen = True
 else:
-    dr = (1152, 864)  # display resolution in px
-    ds = 65  # distance to screen in cm
-    dd = (40.0, 30.0)  # display dimensions in cm ... 39.0 x 29.5
-    windowed = False
+    ## These are here just for the record -- monitor dimensions are stored in PsychoPy monitor center
+    # ds = 65  # distance to screen in cm
+    # dr = (1152, 864)  # display resolution in px
+    # dd = (40.0, 30.0)  # display dimensions in cm ... 39.0 x 29.5
+    screen_name = 'station3'
+    full_screen = True
 # fixation cross:
 fix_size = 0.8
 # stimulus dimensions:
@@ -103,25 +115,18 @@ output_mat = {}
 
 ## Monitor setup
 if shocky:
-    mon = monitors.Monitor('Shocky', width=dd[0], distance=ds)
-    mon.setSizePix(dr)
-    window = visual.Window(dr, monitor=mon, fullscr=False, screen=1, units='deg')
-    # Reassign stimulus durations for the slower refresh rate:
-    fix_dur = fix_dur / 2  # in frames
-    stim_dur = stim_dur / 2
-    beg_buff = beg_buff / 2  # beginning buffer of fixation
-    end_buff = beg_buff
-    wiggle = wiggle / 2  # additional 'wiggle room' for jittering of stimulus onset
-    interval_dur = stim_dur * 2 + beg_buff + end_buff + wiggle  # 17 frames
-    trial_dur = fix_dur + interval_dur * 2  # 44 frames
+    window = visual.Window(monitor=screen_name, fullscr=full_screen, screen=1, color=[-.5, -.5, -.5], units='deg')
 else:
     # TODO make sure that 'station3' monitor profile exists and is properly configured
-    mon = monitors.Monitor('station3')
-    window = visual.Window(dr, fullscr=True, monitor=mon, color=[-.5, -.5, -.5], units='deg',
+    window = visual.Window(fullscr=full_screen, monitor=screen_name, color=[-.5, -.5, -.5], units='deg',
                            allowStencil=True, autoLog=False, screen=0, waitBlanking=False)
 
 if shocky:
-    frame_rate = 60
+    if debug:
+        frame_rate = window.getActualFrameRate()
+        print('frame rate: ' + str(frame_rate))
+    else:
+        frame_rate = 60
 else:
     frame_rate = window.getActualFrameRate()
     print('frame rate: ' + str(frame_rate))
@@ -152,7 +157,6 @@ def frame_skip_check(elapsed_t, elapsed_frames):
 # This is done at every frame update, regardless of trial phase, so predefining:
 def frame_routine():
     flip_time_ = window.flip()
-    fix_cross.draw()
     # Checking for quit (the Esc key)
     if event.getKeys(keyList=['escape']):
         exit_routine()
@@ -198,39 +202,52 @@ for trial in trials:
 
     ## Assigning the trial variables:
 
+    # Token variables:
+    interval_resp_given = False
+    number_resp_given = False
+    ori_resp_given = False
+
     # Randomizing whether the stimuli will appear in the first or second interval:
     stim_interval_second = np.random.randint(2)  # 0 if 1st and 1 if 2nd - used in the loop check (BOOL is faster)
     stim_interval = stim_interval_second + 1  # adding 1 to record
     print('stim_interval=' + str(stim_interval))
 
     # Timing variables:
-    soa = trial['SOA']  # stimulus onset asynchrony, in frames
+    soa = trial['SOA']  # stimulus onset asynchrony, in frames: must be greater than stimulus duration
     if shocky:
         soa = soa / 2
-    # jitter the onset timing for the first or only stimulus, in frames:
+    if debug:
+        soa = stim_dur + 10
+    # Jittering the onset timing for the first or only stimulus, in frames:
     jitter = np.random.randint(wiggle+1)  # the jitter could be zero
     print('jitter=' + str(jitter))
-    # onsets and frames of the stimuli:
+    # Onsets and frames of the stimuli:
     stim1_onset = beg_buff + jitter  # time for first stimulus onsets, in frames, from the onset of the interval
     stim1_twin = np.add(stim1_onset, range(int(stim_dur)))  # stimulus time window, i.e., the frames in which it appears
+    # print('stim1 time window:')
+    # print(stim1_twin)
     stim2_onset = stim1_onset + soa
     stim2_twin = np.add(stim2_onset, range(int(stim_dur)))
+    # print('stim2 time window:')
+    # print(stim2_twin)
 
     # Stimulus contrast:
     stim1_c = trial['stim1_c']  # stimulus contrast, log scaled
-    stim1.contrast = 10 * stim1_c
+    stim1.contrast = 10 ** stim1_c
     stim2_c = trial['stim2_c']
-    stim2.contrast = 10 * stim2_c
+    stim2.contrast = 10 ** stim2_c
 
     # Stimulus orientation:
     angle_diff = trial['angle_diff']  # difference in orientation between stimuli (binary): 0 if same, 1 if diff
     stim1_tiltR = np.random.randint(2)  # 0 if leftward and 1 if rightward tilted
-    stim1.ori = 135 - (90 * stim1_tiltR)  # 135 deg if leftward and 45 deg if rightward
+    stim1.ori = 135 - (90 * stim1_tiltR)  # 135 deg if leftward and 45 deg if rightward; checked 2019-05-15
     if not angle_diff:
         stim2_tiltR = stim1_tiltR
     else:
         stim2_tiltR = 1 - stim1_tiltR
     stim2.ori = 135 - (90 * stim2_tiltR)  # the opposite of the first
+    print('stim1 ori = ' + str(stim1.ori))
+    print('stim2 ori = ' + str(stim2.ori))
 
     ## Presentation phase
 
@@ -242,12 +259,13 @@ for trial in trials:
         # Fixation:
         for cur_fix_frame in range(fix_frames):
             flip_time = frame_routine()
+            fix_cross.draw()
             if debug:
                 print('|', end='')
 
         # Iterating through interval frames:
         for cur_frame in range(interval_frames):
-            # flip_time = frame_routine()
+            flip_time = frame_routine()
 
             # stimulus presentation
             if cur_interval == stim_interval_second:
@@ -262,7 +280,7 @@ for trial in trials:
                 # second stimulus time window (may overlap with the first):
                 elif cur_frame in stim2_twin:
                     if debug:
-                        print('/', end='')
+                        print('\\', end='')
                     # draw the second stimulus
                     stim2.draw()
                 else:
@@ -273,50 +291,77 @@ for trial in trials:
                     print('-', end='')
         if debug:
             print(' ', end='')
+    print('\n', end='')
 
     ## Response phase:
 
-    # Displaying the target and measuring the reaction time.
-    # while not targ_resp_given:
-    #
-    #     # Measuring the time it takes for the behavioural response:
-    #     flip_time = frame_routine()
-    #
-    #     # Measuring time elapsed since the start of the trial:
-    #     trial_elapsed_t = flip_time - trial_t_start
-    #
-    #     # Drawing the target:
-    #     targ.draw()
-    #
-    #     if debug:
-    #         # noinspection PyUnboundLocalVariable
-    #         trial_elapsed_frames += 1
-    #
-    #     ## Monitoring for key presses:
-    #     arrow_keys = event.getKeys(keyList=['left', 'right'])
-    #     if len(arrow_keys) > 0:
-    #         if 'left' in arrow_keys:
-    #             print('subject response: Left')
-    #             beh_resp = -1
-    #             targ_resp_given = True
-    #         else:
-    #             print('subject response: Right')
-    #             beh_resp = 1
-    #             targ_resp_given = True
-    #         if targ_resp_given:  # this is overwritten every time any key is pressed
-    #             rt = flip_time - rt_start
-    #             # noinspection PyUnboundLocalVariable
-    #             if beh_resp == this_targ_loc:
-    #                 corr_resp = 1  # correct location response
-    #                 accuracy_feedback = 'Correct!'
-    #             else:
-    #                 corr_resp = 0  # incorrect location response
-    #                 accuracy_feedback = 'INCORRECT!'
-    #             print('RT=%.2f correct?=%s' % (rt, corr_resp))
-    #             tracker.sendMessage('TRIAL_RESPONSE %.2f' % flip_time)
-    #             if debug:  # in debug mode, check if the frame rate looks okay
-    #                 # noinspection PyUnboundLocalVariable
-    #                 frame_skip_check(trial_elapsed_t, trial_elapsed_frames)
+    # Interval response:
+    while not interval_resp_given:
+
+        window.flip()
+        instr_text_stim.setText('1st or 2nd interval?\n'
+                                'Left = 1st\n'
+                                'Right = 2nd')
+        instr_text_stim.draw()
+
+        ## Monitoring for key presses:
+        arrow_keys = event.getKeys(keyList=['left', 'right'])
+        if len(arrow_keys) > 0:
+            if 'left' in arrow_keys:
+                print('interval response: First')
+                interval_resp = 1
+                interval_resp_given = True
+            else:
+                print('interval response: Second')
+                interval_resp = 2
+                interval_resp_given = True
+
+    # Stimulus number response:
+    while not number_resp_given:
+
+        window.flip()
+        instr_text_stim.setText('1 or 2 stimuli?\n'
+                                'Left = 1\n'
+                                'Right = 2')
+        instr_text_stim.draw()
+
+        ## Monitoring for key presses:
+        arrow_keys = event.getKeys(keyList=['left', 'right'])
+        if len(arrow_keys) > 0:
+            if 'left' in arrow_keys:
+                print('stimulus number response: single')
+                number_resp = 1
+                number_resp_given = True
+            else:
+                print('stimulus number response: double')
+                number_resp = 2
+                number_resp_given = True
+
+    # Orientation response:
+    while not ori_resp_given:
+
+        window.flip()
+        instr_text_stim.setText('Stimulus pattern?\n'
+                                'Left = leftward tilt\n'
+                                'Right = rightward tilt\n'
+                                'Down = plaid')
+        instr_text_stim.draw()
+
+        ## Monitoring for key presses:
+        arrow_keys = event.getKeys(keyList=['left', 'right', 'down'])
+        if len(arrow_keys) > 0:
+            if 'left' in arrow_keys:
+                print('orientation response: leftward tilt')
+                ori_resp = 1
+                ori_resp_given = True
+            elif 'right' in arrow_keys:
+                print('orientation response: rightward tilt')
+                ori_resp = 2
+                ori_resp_given = True
+            elif 'down' in arrow_keys:
+                print('orientation response: plaid')
+                ori_resp = 3
+                ori_resp_given = True
 
     ## Trial termination feedback:
     window.flip()
